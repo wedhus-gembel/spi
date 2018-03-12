9e2a1e259b73966c7b2bda7a3c2b5250446d10e9
wedhus-gembel <za.mi213@gmail.com>
2018-03-12 11:57:03 +0700

    Add files via upload

---
+++ spi/SpiDirectPayment.php
@@ -0,0 +1,48 @@
+<?php
+
+require "Spi.php";
+require "SpiMessage.php";
+require "SpiHelper.php";
+
+class SpiDirectPayment extends Spi {
+	var $spiMessage;
+	var $payment_method;
+	var $encrypt_method;
+
+	public function __construct() {
+		$this->spiMessage = new SpiMessage();
+		$this->encrypt_method = 0;
+	} 
+	
+	public function setEncryptMethod ($method = 0){
+		$this->encrypt_method = $method;
+	}
+
+	public function setMessageFromJson($json){
+		$this->spiMessage->create_spi_message($json);
+		$this->spiMessage->set_item($this->spiMessage->_spi_token, $this->PRIVATE_KEY1.$this->PRIVATE_KEY2);
+	}
+
+	public function setPaymentMethod ($payment_method = ""){
+		$this->payment_method = $payment_method;
+	}
+
+	public function doPay(){
+		$message = $this->spiMessage->getJson();
+		$token = $this->getToken();
+		$result = "";
+		if($this->encrypt_method == 0){
+			$messageEncrypted = SpiHelper::encrypt($message, $token);
+			$messageEncrypted = substr($messageEncrypted, 0, 10). $token. substr($messageEncrypted, 10);
+			$messagePay = array("orderdata" => $messageEncrypted);
+			$result = $this->doPost(SCApiConstant::PATH_API . "/". $this->payment_method, $messagePay);
+		} else {
+			$messageEncrypted = SpiHelper::OpenSSLEncrypt($message, $token);
+			$messageEncrypted = substr($messageEncrypted, 0, 10). $token. substr($messageEncrypted, 10);
+			$messagePay = array("orderdata" => $messageEncrypted);
+			$result = $this->doPost(SCApiConstant::PATH_API2 . "/". $this->payment_method, $messagePay);
+		}
+		return $result;
+	}
+
+}
---
+++ spi/SpiHelper.php
@@ -0,0 +1,58 @@
+<?php
+
+Class SpiHelper {
+
+	public static function encrypt($text, $key) {
+	    $key = $key;
+	    return trim(base64_encode(
+	    mcrypt_encrypt(MCRYPT_RIJNDAEL_256,
+	    $key, $text, MCRYPT_MODE_ECB,
+	    mcrypt_create_iv(
+	    mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256,
+	    MCRYPT_MODE_ECB), MCRYPT_RAND))));
+	}
+
+	public static function decrypt($text, $key) {
+	    return trim(
+	            mcrypt_decrypt(
+	                MCRYPT_RIJNDAEL_256,
+	                $key, base64_decode($text), MCRYPT_MODE_ECB,
+	                    mcrypt_create_iv(
+	                        mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256,
+	                        MCRYPT_MODE_ECB), 
+	                MCRYPT_RAND)
+	            ));
+	     
+
+
+	}
+
+
+	public static function OpenSSLEncrypt($message, $key) {
+	    $output = false;
+	    $encrypt_method = "AES-256-CBC";
+	    $secret_key = $key;
+	    $secret_iv = $key;
+	    // hash
+	    $key = hash('sha256', $secret_key);
+	    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+	    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+	    $output = openssl_encrypt($message, $encrypt_method, $key, 0, $iv);
+	    $output = trim(base64_encode($output));
+	    return $output;
+	}
+
+	public static function OpenSSLDecrypt($message, $key) {
+	    $output = false;
+	    $encrypt_method = "AES-256-CBC";
+	    $secret_key = $key;
+	    $secret_iv = $key;
+	    // hash
+	    $key = hash('sha256', $secret_key);
+	    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+	    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+	    $output = trim(openssl_decrypt(base64_decode($message), $encrypt_method, $key, 0, $iv));
+	    return $output;
+	}
+
+}
